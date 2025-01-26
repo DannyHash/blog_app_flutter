4e6e8ce9042e3340a7b0ba28e69b5fb4ffade7b1
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-27 00:56:28 +0100

    Added sign-in prompt to SignupPage with styled RichText

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app_flutter/core/theme/app_pallete.dart';
 import 'package:blog_app_flutter/features/auth/presentation/widgets/auth_field.dart';
 import 'package:blog_app_flutter/features/auth/presentation/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,7 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(15.0),
         child: Column(
@@ -33,6 +34,21 @@ class _SignupPageState extends State<SignupPage> {
             AuthField(hintText: 'Pssword'),
             SizedBox(height: 20),
             AuthGradientButton(),
+            SizedBox(height: 20),
+            RichText(
+              text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            color: AppPallete.gradient2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ]),
+            )
           ],
         ),
       ),
