6f5a6f72beb2d612d29ebcb78c5264929657bb4a
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-27 00:41:10 +0100

    Refactored AuthGradientButton to use gradient background and updated styling

--- lib/features/auth/presentation/widgets/auth_gradient_button.dart
+++ lib/features/auth/presentation/widgets/auth_gradient_button.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app_flutter/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AuthGradientButton extends StatelessWidget {
@@ -5,9 +6,33 @@ class AuthGradientButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text('Sign Up'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            AppPallete.gradient1,
+            AppPallete.gradient2,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(395, 55),
+          backgroundColor: AppPallete.transparentColor,
+          shadowColor: AppPallete.transparentColor,
+        ),
+        child: Text(
+          'Sign Up',
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
     );
   }
 }
