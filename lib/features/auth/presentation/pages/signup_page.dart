905cde34400f49fc11449c9b2049a6c324736716
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-27 01:55:13 +0100

    Added form validation and controllers to SignupPage and AuthField widget

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -11,45 +11,72 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final emialController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emialController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sign Up.',
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
+        child: Form(
+          child: Column(
+            key: formKey,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign Up.',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            AuthField(hintText: 'Name'),
-            SizedBox(height: 15),
-            AuthField(hintText: 'Email'),
-            SizedBox(height: 15),
-            AuthField(hintText: 'Pssword'),
-            SizedBox(height: 20),
-            AuthGradientButton(),
-            SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                  text: 'Don\'t have an account? ',
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      text: 'Sign In',
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: AppPallete.gradient2,
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                  ]),
-            )
-          ],
+              SizedBox(height: 30),
+              AuthField(
+                hintText: 'Name',
+                controller: nameController,
+              ),
+              SizedBox(height: 15),
+              AuthField(
+                hintText: 'Email',
+                controller: emialController,
+              ),
+              SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              SizedBox(height: 20),
+              AuthGradientButton(),
+              SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                    text: 'Don\'t have an account? ',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    children: [
+                      TextSpan(
+                        text: 'Sign In',
+                        style:
+                            Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  color: AppPallete.gradient2,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                      ),
+                    ]),
+              )
+            ],
+          ),
         ),
       ),
     );
