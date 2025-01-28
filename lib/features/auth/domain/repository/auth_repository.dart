0da3e0a0f6dfcdb3b37ca944ebcb0c62753466e0
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-28 12:54:05 +0100

    Added Failure class and AuthRepository interface for authentication handling

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,14 @@
+import 'package:blog_app_flutter/core/error/failures.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signInWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, String>> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+}
