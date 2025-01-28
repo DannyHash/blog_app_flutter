433148d6640bf8da115b3d9eef800afff01e83eb
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-28 15:51:53 +0100

    Added Supabase dependency, configured plugin registrations, and initialized Supabase in main function

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://oulypvzfpgncgrqmeoiz.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91bHlwdnpmcGduY2dycW1lb2l6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwNjU1MzYsImV4cCI6MjA1MzY0MTUzNn0.phWG2G3czoulxUSds71awa-mbAMivyAnYtgrTb_na-s';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:blog_app_flutter/core/secrets/app_secrets.dart';
 import 'package:blog_app_flutter/core/theme/theme.dart';
 import 'package:blog_app_flutter/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
