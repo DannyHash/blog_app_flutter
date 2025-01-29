466f0fcd4b7b7d09689fa7f9e22c019bf3d6de25
Danny-Hash <dannyhash0x1@gmail.com>
2025-01-29 02:44:34 +0100

    Added AuthRemoteDataSource implementation and ServerException class for authentication

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -2,7 +2,7 @@ import 'package:blog_app_flutter/core/error/failures.dart';
 import 'package:fpdart/fpdart.dart';
 
 abstract interface class AuthRepository {
-  Future<Either<Failure, String>> signInWithEmailAndPassword({
+  Future<Either<Failure, String>> signUpWithEmailAndPassword({
     required String name,
     required String email,
     required String password,
